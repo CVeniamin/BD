f4227edfe2a2142efc65a2443a596fad073c8d93
Che <veniamin.craciun@tecnico.ulisboa.pt>
2016-09-29 18:44:09 +0100

    bug error

--- test.php
+++ test.php
@@ -1,15 +1,17 @@
+<!DOCTYPE html>
 <html>
   <head>
+    <meta charset="utf-8">
     <title>First php on delta</title>
   </head>
   <body>
-  <?php
+  <? php
   	$host="db.ist.utl.pt";
   	$user="ist182028";
   	$password="aaqq2196";
   	$dbname = $user;
 
-  	$connection = new PDO("mysql:host=" . $host. ";dbname=" . $dbname, $user, $password, array(PDO::ATTR_ERRMODE =&gt; PDO::ERRMODE_WARNING));
+  	$connection = new PDO("mysql:host=" . $host. ";dbname=" . $dbname, $user, $password, array(PDO::ATTR_ERRMODE =>PDO::ERRMODE_WARNING));
 
   	echo("<p>Connected to MySQL database $dbname on $host as user $user</p>\n");
 
@@ -29,20 +31,16 @@
   	{
   		echo("\n");
   	}
-  	echo("<table border="\&quot;1\&quot;"><tbody><tr><td>account_number</td><td>branch_name</td><td>balance</td></tr><tr><td>");
-  		echo($row["account_number"]);
-  		echo("</td><td>");
-  		echo($row["branch_name"]);
-  		echo("</td><td>");
-  		echo($row["balance"]);
-  		echo("</td></tr></tbody></table>\n");
-
-          $connection = null;
-
+  	echo("<table border="1"><tbody><tr><td>account_number</td><td>branch_name</td><td>balance</td></tr><tr><td>");
+		echo($row["account_number"]);
+		echo("</td><td>");
+		echo($row["branch_name"]);
+		echo("</td><td>");
+		echo($row["balance"]);
+		echo("</td></tr></tbody></table>\n");
+    $connection = null;
   	echo("<p>Connection closed.</p>\n");
-
   	echo("<p>Test completed successfully.</p>\n");
-
     ?>
     <p>
       hue!
