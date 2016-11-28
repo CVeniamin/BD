87d0554b7dd593ac87fb7cb1b877e2f578aa380d
Veniamin Craciun <veniamin.craciun@tecnico.ulisboa.pt>
2016-11-28 14:43:29 +0000

    user to utilizador

--- populate.sql
+++ populate.sql
@@ -1,12 +1,12 @@
-insert into user values (950245011,'Chaim Tobias', 969885689);
-insert into user values (354396861,'Iser Rapp',918876950);
-insert into user values (366165710,'Agam Kauffmann',929848162);
-insert into user values (808866092,'Yussel Schenck', 930678582);
-insert into user values (822402323,'Ber Landau',8000000);
-insert into user values (822402583,'Yachin Morgenstern', 967092724);
-insert into user values (362985512,'Udi Lehrer',927785432);
-insert into user values (842505457,'Ziv Harel',934937561);
-insert into user values (902250809,'Mor Rothenberg', 918071174);
+insert into utilizador values (950245011,'Chaim Tobias', 969885689);
+insert into utilizador values (354396861,'Iser Rapp',918876950);
+insert into utilizador values (366165710,'Agam Kauffmann',929848162);
+insert into utilizador values (808866092,'Yussel Schenck', 930678582);
+insert into utilizador values (822402323,'Ber Landau',8000000);
+insert into utilizador values (822402583,'Yachin Morgenstern', 967092724);
+insert into utilizador values (362985512,'Udi Lehrer',927785432);
+insert into utilizador values (842505457,'Ziv Harel',934937561);
+insert into utilizador values (902250809,'Mor Rothenberg', 918071174);
 
 insert into edificio values ('A');
 insert into edificio values ('B');
