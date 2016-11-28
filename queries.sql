8937c65d4c4decfe12dbf377c0886d44b7acb9b3
Veniamin Craciun <veniamin.craciun@tecnico.ulisboa.pt>
2016-11-28 23:05:40 +0000

    Remove unnecessary code

--- queries.sql
+++ queries.sql
@@ -48,19 +48,3 @@ FROM posto p LEFT JOIN aluga a ON a.morada = p.morada AND a.codigo = p.codigo
   LEFT JOIN estado e ON e.numero = a.numero
 WHERE e.estado = 'aceite'
 GROUP BY p.morada, p.codigo;
-
-
-
-
--- c)
--- gettin total reserves for each alugavel
-SELECT morada, count(codigo) FROM aluga WHERE 1 GROUP BY morada;
-
-SELECT morada, codigo FROM (SELECT morada, codigo, count(codigo) as num FROM aluga WHERE 1 GROUP BY codigo) as total_reserves WHERE num >= (select avg(num) from total_reserves);
-
-
--- b)
--- example made by le me
-# select customer_name from ((select customer_name, count(account_number) as num from depositor group by customer_name) as total) natural join (select avg(num) as avg_num from (select customer_name, count(account_number) as num from depositor group by customer_name) as avg_table) as average where num > avg_num;
-
-select morada from ((select morada, count(codigo) as num from aluga group by morada) as total) natural join (select avg(num) as avg_num from (select morada, count(codigo) as num from aluga group by morada) as avg_table) as average where num > avg_num;
